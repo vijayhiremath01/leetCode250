923e9a3c260b61a075502aad24e5278db7248cac
vijay18vp <voidprotocol18@gmail.com>
2025-12-12 22:40:01 +0530

    Optimized approach for containsDuplicate

--- 2.containsDuplicate/containsDuplicate.java
+++ 2.containsDuplicate/containsDuplicate.java
@@ -3,23 +3,34 @@ import java.util.*;
 public class containsDuplicate {
     public boolean ContainsDuplicate(int[] nums){
         HashSet<Integer> seenNumbers = new HashSet<>();
-        // We are iterating through each element in an array 
+
         for(int num : nums){
-            // If the number is already in the set, we have a duplicate
             if(seenNumbers.contains(num)){
+                // How much time it is repeated
+                System.out.println("The number " + num + " is repeated.");
                 return true;
             }
-            // Add the number to hashSet
             seenNumbers.add(num);
         }
-        // If we dont find any duplicates, return false
         return false;
     }
 
     public static void main(String[] args) {
         containsDuplicate obj = new containsDuplicate();
-        int[] nums = {1 , 4, 5, 6,7 ,8 };
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];   // FIXED
+
+        System.out.println("Enter the elements of the array : ");
+        for(int i = 0; i < size; i++){
+            nums[i] = sc.nextInt();
+        }
+
         boolean result = obj.ContainsDuplicate(nums);
         System.out.println(result);
+        sc.close();
     }
 }
