dda544a8506feb5537baf894a458455ad229ee68
vijay18vp <voidprotocol18@gmail.com>
2025-12-17 20:52:42 +0530

    linearSearch problem date - 17th dec

---
+++ 4.linearSearch/linearSearch.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+
+public class linearSearch {
+    // return true if element found in array
+    public static boolean linearSearch(int[] array , int target){
+        // check if length of array is zero
+        if(array.length == 0){
+            return false ;
+        }
+// check with loop
+      for(int element : array){
+          if(element == target){
+              return true ;
+          }
+      }
+      return false ;
+    }
+
+
+    public static void main(String[] args){
+        int[] array = {12 , 34 , 7 , 89 , 67 , 45};
+        int target ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a target : ");
+        target = sc.nextInt();
+        boolean ans = linearSearch(array , target);
+        System.out.println(ans);
+        sc.close();
+    }
+}
