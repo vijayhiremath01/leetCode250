81fbbf70c4ec558863c68cd08a3bfb3403af4f8a
vijay18vp <voidprotocol18@gmail.com>
2026-01-08 21:29:33 +0530

    Some Questions solved

--- 2.containsDuplicate/containsDuplicate.java
+++ 2.containsDuplicate/containsDuplicate.java
@@ -15,7 +15,7 @@ public class containsDuplicate {
         return false;
     }
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         containsDuplicate obj = new containsDuplicate();
         Scanner sc = new Scanner(System.in);
 
--- 4.linearSearch/linearSearch.java
+++ 4.linearSearch/linearSearch.java
@@ -1,29 +1,28 @@
-import java.util.* ;
+import java.util.*;
 
 public class linearSearch {
     // return true if element found in array
-    public static boolean linearSearch(int[] array , int target){
+    public static boolean search(int[] array, int target) {
         // check if length of array is zero
-        if(array.length == 0){
-            return false ;
+        if (array.length == 0) {
+            return false;
         }
-// check with loop
-      for(int element : array){
-          if(element == target){
-              return true ;
-          }
-      }
-      return false ;
+        // check with loop
+        for (int element : array) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
     }
 
-
-    public static void main(String[] args){
-        int[] array = {12 , 34 , 7 , 89 , 67 , 45};
-        int target ;
+    public static void main(String[] args) {
+        int[] array = { 12, 34, 7, 89, 67, 45 };
+        int target;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a target : ");
         target = sc.nextInt();
-        boolean ans = linearSearch(array , target);
+        boolean ans = search(array, target);
         System.out.println(ans);
         sc.close();
     }
