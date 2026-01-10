72e1ab8c25916d183f7baae01caf66c9e759a82e
vijay18vp <voidprotocol18@gmail.com>
2026-01-10 11:48:38 +0530

    pluOne problem solved

--- 7.MergeTwo/linked2.java
+++ 7.MergeTwo/linked2.java
@@ -1,12 +1,12 @@
-import java.util.* ;
 
 class Node {
     String data;
-    Node next ;
+    Node next;
 }
+
 public class linked2 {
     public static void main(String[] args) {
-       // Creating nodes
+        // Creating nodes
         Node n1 = new Node();
         n1.data = "Vijay";
 
@@ -16,23 +16,23 @@ public class linked2 {
         Node n3 = new Node();
         n3.data = "Hiremath";
 
-        n1.next = n2 ;
-        n2.next = n3 ;
-        n3.next = null ;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = null;
 
         // Inserting the new node at first
         Node newNode = new Node();
         newNode.data = "Shaila";
 
-        Node head = n1 ;
-        newNode.next = head ;
+        Node head = n1;
+        newNode.next = head;
         head = newNode;
 
         // Traverse the data
-        Node temp = head ;
-        while(temp != null){
+        Node temp = head;
+        while (temp != null) {
             System.out.println(temp.data);
-            temp = temp.next ;
+            temp = temp.next;
         }
         System.out.println(" ");
     }
---
+++ 8.plusOne/brute.java
@@ -0,0 +1,45 @@
+import java.util.* ;
+public class brute {
+ // creating a method for plusOne
+    public static int[] plusOne(int[] array){
+        String num = ""; // initialising the string
+
+        for(int d : array){
+             num += d ;
+        }
+
+        // again sting to number and then adding 1 so that n + 1
+        long n = Long.parseLong(num);
+        n = n + 1 ;  // plus One the number means 123 + 1 ----> 124 example just
+
+        // Number ----> String for convert the string to array
+        String s = String.valueOf(n);
+        int[] result = new int[s.length()];
+
+        for(int i = 0 ; i < s.length() ; i++){
+            result[i] = s.charAt(i) - '0';
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+
+              Scanner sc = new Scanner(System.in);
+
+              System.out.println("Enter the size of an array : ");
+              int n = sc.nextInt();
+              int[] array = new int[n] ;
+
+              System.out.println("Enter the number into the array : ");
+              for(int i = 0 ; i < n ; i++){
+                  array[i] = sc.nextInt();
+              }
+
+              // Calling the function to plusOne
+          int[] result = plusOne(array);
+              System.out.println("The Array after the plusOne is : " );
+              for(int i = 0 ; i < result.length ; i++){
+                  System.out.print(result[i] + " ");
+              }
+    }
+}
