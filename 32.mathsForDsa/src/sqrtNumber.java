4f2482bb1e7d575718baf6b01b13ff13deef74cc
vijay18vp <voidprotocol18@gmail.com>
2026-05-02 09:11:35 +0530

    Finding the SQUARE ROOT OF A Number

---
+++ 32.mathsForDsa/src/sqrtNumber.java
@@ -0,0 +1,44 @@
+import java.util.* ;
+
+public class sqrtNumber {
+
+    public static double sqrt(int n , int p){
+         int start = 0 ;
+         int end = n ;
+
+         double root = 0.0 ;
+
+         while(start <= end){
+             int mid = start + (end - start)/2 ;
+
+             if(mid*mid == n){
+                 return mid ;
+             } else if(mid*mid > n){
+                 end  = mid - 1 ;
+             } else {
+                 start = mid + 1 ;
+             }
+         }
+
+         double incm = 0.1 ;
+         for(int i = 0 ; i < p ; i++){
+             while(root*root <= n){
+                  root += incm ;
+             }
+
+             root-= incm ;
+             incm /= 10 ;
+         }
+
+         return root;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int p = s.nextInt();
+        double result = sqrt(n,p);
+        System.out.println(result);
+    }
+}
