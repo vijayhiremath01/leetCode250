a1126aa0d05f4cc5cdf1cb7db57d3db8275ab12d
vijay18vp <voidprotocol18@gmail.com>
2026-05-04 21:55:03 +0530

    Learned and Solved Leetcode Problem of strStr

---
+++ 36.strStr/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.* ;
+
+public class Solution {
+
+    public static int strStr(String haystack , String needle){
+        char[] arr1 = haystack.toCharArray();
+        char[] arr2 = needle.toCharArray();
+
+        for(int i = 0 ; i <= arr1.length - arr2.length ; i++){
+            boolean match = true ;
+            for(int j = 0 ; j < arr2.length ; j++){
+                if(arr1[i+j] != arr2[j]){
+                    match = false ;
+                    break ;
+                }
+            }
+            if(match){
+                return i ;
+            }
+        }
+        return -1 ;
+    }
+
+    public static void main(String[] args) {
+
+        String haystack = "DERabc" ;
+        String needle = "abc" ;
+        System.out.println(strStr(haystack, needle));
+
+    }
+}
