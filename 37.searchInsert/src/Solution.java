d32c27b0bd3f4c31a023dac6c8562109cc08fcd5
vijay18vp <voidprotocol18@gmail.com>
2026-05-05 20:02:56 +0530

    Solved LeetCode problem for searchInsert and Binary Search concept

---
+++ 37.searchInsert/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.* ;
+
+public class Solution {
+
+    public static int searchInsert(int[] nums , int target) {
+        if(nums.length == 0){
+            return -1 ;
+        }
+
+        int start = 0 ;
+        int end = nums.length - 1 ;
+
+        while(start <= end){
+            int mid = start + (end - start)/2 ;
+
+            if(nums[mid] == target){
+                return mid ;
+            } else if(nums[mid] < target){
+                start = mid + 1 ;
+            } else {
+                end = mid - 1 ;
+            }
+        }
+        return start ;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,5,6};
+
+        int target = 2 ;
+        System.out.println(searchInsert(nums, target));
+
+    }
+
+}
